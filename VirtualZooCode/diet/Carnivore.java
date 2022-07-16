a196c14c446b1aae3c9825bec5ee7184384b5d6b
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-07-16 10:36:40 +0300

    Adjusting notes

--- VirtualZooCode/diet/Carnivore.java
+++ VirtualZooCode/diet/Carnivore.java
@@ -1,10 +1,17 @@
 package diet;
 
-
 import animals.Animal;
 import food.EFoodType;
 import food.IEdible;
 
+
+/**
+ * A class representing the Carnivores, and implement IDiet
+ *
+ * @version 2020.3.3
+ * @author Nitzan Tomer
+ * @see diet.IDiet
+ */
 public class Carnivore implements IDiet {
 
     /**
@@ -16,10 +23,26 @@ public class Carnivore implements IDiet {
     public Carnivore() {}
 
 
+    /**
+     * Getting a food, and check if Carnivore can eat it
+     * Notes : Carnivore may eat only MEAT
+     *
+     * @param food is a String representing the food type (food type define in EFoodType)
+     * @see food.EFoodType
+     * @return True if the food is MEAT, else False
+     */
     @Override
     public boolean canEat(EFoodType food) { return food == EFoodType.MEAT; }
 
 
+    /**
+     * Getting Animal object and a food, check if the object may eat it using the canEat method,
+     * If yes, calculate the gained weight, else the gained weight is None
+     *
+     * @param animal is an Animal representing animal object
+     * @param food is a String representing the food type (food type is from EFoodType)
+     * @return a Double representing the gained weight calculation
+     */
     @Override
     public double eat(Animal animal, IEdible food) {
         if (canEat(food.getFoodType()))
