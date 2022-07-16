9aab47c81d88af8659c435ca34bb9a7c4fcb1cda
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-07-16 10:35:46 +0300

    Adding implementation of IDiet interface
    Implementing canEat and eat methods of the IDiet interface

--- VirtualZooCode/diet/Carnivore.java
+++ VirtualZooCode/diet/Carnivore.java
@@ -1,6 +1,10 @@
 package diet;
 
 
+import animals.Animal;
+import food.EFoodType;
+import food.IEdible;
+
 public class Carnivore implements IDiet {
 
     /**
@@ -10,4 +14,17 @@ public class Carnivore implements IDiet {
      * @see animals.Animal
      */
     public Carnivore() {}
+
+
+    @Override
+    public boolean canEat(EFoodType food) { return food == EFoodType.MEAT; }
+
+
+    @Override
+    public double eat(Animal animal, IEdible food) {
+        if (canEat(food.getFoodType()))
+            return animal.getWeight() * 0.1;
+
+        return 0.0;
+    }
 }
