80351c642f2ffe853c1a4db3d29ea21e729c553a
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-07-16 10:33:06 +0300

    Adding new diet type "Carnivore"

---
+++ VirtualZooCode/diet/Carnivore.java
@@ -0,0 +1,13 @@
+package diet;
+
+
+public class Carnivore implements IDiet {
+
+    /**
+     * The constructor of the Carnivore object, Sets the attributes of the object
+     * Note : The object itself has no field, but he used as one
+     *
+     * @see animals.Animal
+     */
+    public Carnivore() {}
+}
