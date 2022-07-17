94da97e644eae6eff73f65f054cdaa48d7ea2a9e
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-07-17 08:49:42 +0300

    Adjusting notes

--- VirtualZooCode/diet/IDiet.java
+++ VirtualZooCode/diet/IDiet.java
@@ -5,6 +5,14 @@ import food.EFoodType;
 import food.IEdible;
 
 
+/**
+ * Interface to describe the functionality of eating
+ * Note: This interface used as a field to describe the functionality of eating
+ *
+ * @version 2020.3.3
+ * @author Nitzan Tomer
+ * @see animals.Animal
+ */
 public interface IDiet {
 
     boolean canEat(EFoodType food);
