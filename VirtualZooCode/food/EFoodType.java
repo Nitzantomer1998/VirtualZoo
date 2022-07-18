455249e9b6d5afa8526031ac641a69abe38ca16a
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-07-18 08:49:25 +0300

    Adjusting notes

--- VirtualZooCode/food/EFoodType.java
+++ VirtualZooCode/food/EFoodType.java
@@ -1,4 +1,10 @@
 package food;
 
 
+/**
+ * Enum file to save types of food that available
+ *
+ * @version 2020.3.3
+ * @author Nitzan Tomer
+ */
 public enum EFoodType { MEAT, NOTFOOD, VEGETABLE }
