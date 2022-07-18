4787f89f6cfb9a608f1f453aeec0711c3d602df5
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-07-18 08:47:36 +0300

    Adjusting notes

--- VirtualZooCode/food/IEdible.java
+++ VirtualZooCode/food/IEdible.java
@@ -1,8 +1,12 @@
 package food;
 
-import food.EFoodType;
 
-
+/**
+ * Interface to describe the Food Type of the object
+ *
+ * @version 2020.3.3
+ * @author Nitzan Tomer
+ */
 public interface IEdible {
 
     EFoodType getFoodType();
