29447564dd385a043b64a4cf133d907a7aa010e8
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-07-19 09:18:37 +0300

    Adjusting notes

--- VirtualZooCode/mobility/ILocatable.java
+++ VirtualZooCode/mobility/ILocatable.java
@@ -1,6 +1,12 @@
 package mobility;
 
 
+/**
+ * Interface to describe place functionality
+ *
+ * @version 2020.3.3
+ * @author Nitzan Tomer
+ */
 public interface ILocatable {
 
     void setLocation(Point newLocation);
