37951de568a86db53717daebb3bf83c27e447f50
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-07-19 09:21:52 +0300

    Adjusting notes

--- VirtualZooCode/mobility/Point.java
+++ VirtualZooCode/mobility/Point.java
@@ -1,6 +1,12 @@
 package mobility;
 
 
+/**
+ * Class to define position on a two-dimensional Axis
+ *
+ * @version 2020.3.3
+ * @author Nitzan Tomer
+ */
 public class Point {
 
     private static final int MAX_X = 800;
@@ -10,12 +16,23 @@ public class Point {
     private final int y;
 
 
+    /**
+     * The constructor of the Point object, Sets the attributes of the object
+     *
+     * @param x is an Integer representing the location on axis X
+     * @param y is an Integer representing the location on axis Y
+     */
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
 
+    /**
+     * Getter methods for the fields of the object / class
+     *
+     * @return The wanted attribute
+     */
     public int getX() { return this.x; }
     public int getY() { return this.y; }
     public static int getMaxX() { return Point.MAX_X; }
