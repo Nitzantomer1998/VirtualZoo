537ffa50674ae7a6c4327879cdb8f1a3f2e971c1
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-07-20 09:02:34 +0300

    Adjusting notes

--- VirtualZooCode/foodSupply/Meat.java
+++ VirtualZooCode/foodSupply/Meat.java
@@ -3,17 +3,35 @@ package foodSupply;
 import food.EFoodType;
 
 
+/**
+ * A class representing a Meat, and inherit from AvailableFood
+ *
+ * @version 2020.3.3
+ * @author Nitzan Tomer
+ * @see foodSupply.AvailableFood
+ */
 public class Meat extends AvailableFood {
 
     public static volatile Meat instance = null;
 
 
+    /**
+     * The constructor of the Meat object, Sets the fields of the object
+     */
     private Meat() {
         super();
         this.loadImages("meat.png");
     }
 
 
+    /**
+     * Static method for ensuring there's maximum of one instance type of this class,
+     * If there's no instance it will create one, else it will return the existing instance,
+     * Note: Also updating the static class field of AvailableFood to be this instance
+     * Note: Singleton DP + little tweaks
+     *
+     * @return Instance of this class
+     */
     public static Meat getInstance() {
         if (Meat.instance == null)
             synchronized (Meat.class) {
@@ -28,6 +46,12 @@ public class Meat extends AvailableFood {
     }
 
 
+    /**
+     * Getter method for the food type of the object
+     *
+     * @see food.IEdible
+     * @return The object's food type
+     */
     @Override
     public EFoodType getFoodType() { return EFoodType.MEAT; }
 }
