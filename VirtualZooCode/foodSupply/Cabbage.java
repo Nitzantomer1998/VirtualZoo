bf1b72b841a5ad663cd2901cc28daa94b247f48e
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-07-20 08:59:20 +0300

    Adjusting notes

--- VirtualZooCode/foodSupply/Cabbage.java
+++ VirtualZooCode/foodSupply/Cabbage.java
@@ -3,17 +3,35 @@ package foodSupply;
 import food.EFoodType;
 
 
+/**
+ * A class representing a Cabbage, and inherit from AvailableFood
+ *
+ * @version 2020.3.3
+ * @author Nitzan Tomer
+ * @see foodSupply.AvailableFood
+ */
 public class Cabbage extends AvailableFood {
 
     public static volatile Cabbage instance = null;
 
 
+    /**
+     * The constructor of the cabbage object, Sets the fields of the object
+     */
     private Cabbage() {
         super();
         this.loadImages("cabbage.png");
     }
 
 
+    /**
+     * Static method for ensuring there's maximum of one instance type of this class,
+     * If there's no instance it will create one, else it will return the existing instance,
+     * Note: Also updating the static class field of AvailableFood to be this instance
+     * Note: Singleton DP + little tweaks
+     *
+     * @return Instance of this class
+     */
     public static Cabbage getInstance() {
         if (Cabbage.instance == null)
             synchronized (Cabbage.class) {
@@ -28,6 +46,12 @@ public class Cabbage extends AvailableFood {
     }
 
 
+    /**
+     * Getter method for the food type of the object
+     *
+     * @see food.IEdible
+     * @return The object's food type
+     */
     @Override
     public EFoodType getFoodType() { return EFoodType.VEGETABLE; }
 }
