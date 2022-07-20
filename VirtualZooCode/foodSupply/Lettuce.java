7a03dc08c02842e16d2d7c41b9dc03d9dc33b109
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-07-20 09:01:11 +0300

    Adjusting notes

--- VirtualZooCode/foodSupply/Lettuce.java
+++ VirtualZooCode/foodSupply/Lettuce.java
@@ -3,17 +3,35 @@ package foodSupply;
 import food.EFoodType;
 
 
+/**
+ * A class representing a Lettuce, and inherit from AvailableFood
+ *
+ * @version 2020.3.3
+ * @author Nitzan Tomer
+ * @see foodSupply.AvailableFood
+ */
 public class Lettuce extends AvailableFood {
 
     public static volatile Lettuce instance = null;
 
 
+    /**
+     * The constructor of the lettuce object, Sets the fields of the object
+     */
     private Lettuce() {
         super();
         this.loadImages("lettuce.png");
     }
 
 
+    /**
+     * Static method for ensuring there's maximum of one instance type of this class,
+     * If there's no instance it will create one, else it will return the existing instance,
+     * Note: Also updating the static class field of AvailableFood to be this instance
+     * Note: Singleton DP + little tweaks
+     *
+     * @return Instance of this class
+     */
     public static Lettuce getInstance() {
         if (Lettuce.instance == null)
             synchronized (Lettuce.class) {
@@ -28,6 +46,12 @@ public class Lettuce extends AvailableFood {
     }
 
 
+    /**
+     * Getter method for the food type of the object
+     *
+     * @see food.IEdible
+     * @return The object's food type
+     */
     @Override
     public EFoodType getFoodType() { return EFoodType.VEGETABLE; }
 }
