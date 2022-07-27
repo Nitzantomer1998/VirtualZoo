3b198f6b21a1ec9b65bb6bdd7a7bf83f74624384
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-07-27 08:46:24 +0300

    Adjusting notes

--- VirtualZooCode/graphics/IDrawable.java
+++ VirtualZooCode/graphics/IDrawable.java
@@ -3,6 +3,12 @@ package graphics;
 import java.awt.*;
 
 
+/**
+ * Interface to describe the draw functionality
+ *
+ * @version 2020.3.3
+ * @author Nitzan Tomer
+ */
 public interface IDrawable {
 
     String PICTURE_PATH = System.getProperty("user.dir").concat("\\Pictures\\");
