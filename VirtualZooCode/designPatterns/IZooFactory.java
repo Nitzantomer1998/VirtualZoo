fa376e9513bf8ba54cd077b4b6e31fccdffb108a
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-08-06 08:41:19 +0300

    Adjusting notes

--- VirtualZooCode/designPatterns/IZooFactory.java
+++ VirtualZooCode/designPatterns/IZooFactory.java
@@ -3,6 +3,13 @@ package designPatterns;
 import animals.Animal;
 
 
+/**
+ * an Interface to implement the AbstarctFactory Design Pattern
+ *
+ * @version 2020.3.3
+ * @author Nitzan Tomer
+ * @see designPatterns.OmnivoreFactory, designPatterns.HerbivoreFactory, designPatterns.CarnivoreFactory
+ */
 public interface IZooFactory {
 
     Animal createAnimal(String type, String name, double size, int horizontalSpeed, int verticalSpeed, String color);
