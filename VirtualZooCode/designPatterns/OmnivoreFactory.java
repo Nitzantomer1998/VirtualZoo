91d7cd15a7d11471e9fa88aa595f39fc19af1f79
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-08-11 11:57:07 +0300

    Adjusting notes

--- VirtualZooCode/designPatterns/OmnivoreFactory.java
+++ VirtualZooCode/designPatterns/OmnivoreFactory.java
@@ -4,11 +4,34 @@ import animals.Animal;
 import animals.Bear;
 
 
+/**
+ * a Class that defines the Factory Design pattern,
+ * General class of diet Omnivore in order to create instances of it by implement the interface IZooFactory
+ *
+ * @version 2020.3.3
+ * @author Nitzan Tomer
+ * @see designPatterns.IZooFactory
+ */
 public class OmnivoreFactory implements IZooFactory {
 
+    /**
+     * The constructor of the OmnivoreFactory object, Sets the fields of the object
+     *
+     * @see designPatterns.DietFactory
+     */
     public OmnivoreFactory() {}
 
-    
+
+    /**
+     * Implementation of the IZooFactory methods
+     *
+     * @param type is a String representing the type of the animal
+     * @param name is a String representing the name of the animal
+     * @param size is an Integer representing the size of the animal
+     * @param horizontalSpeed is an Integer representing the Horizontal speed of the animal
+     * @param verticalSpeed is an Integer representing the Vertical speed of the animal
+     * @param color is a String representing the color of the animal
+     */
     @Override
     public Animal createAnimal(String type, String name, double size, int horizontalSpeed, int verticalSpeed, String color) {
         if(type.equals("Bear"))
