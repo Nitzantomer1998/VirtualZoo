8f0d219d2f80879117ecff01166b90541e0653c4
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-08-12 11:09:37 +0300

    Adusting notes

--- VirtualZooCode/designPatterns/Controller.java
+++ VirtualZooCode/designPatterns/Controller.java
@@ -5,11 +5,27 @@ import java.util.Observable;
 import java.util.Observer;
 
 
+/**
+ * a Class that defines the Observer Design pattern,
+ * Using the observer in order to update the main frame everytime something has changed
+ *
+ * @version 2020.3.3
+ * @author Nitzan Tomer
+ * @see mobility.Mobile
+ */
 public class Controller implements Observer {
 
+    /**
+     * The constructor of the Controller object, Sets the fields of the object
+     */
     public Controller() {}
 
 
+    /**
+     * Method to update all the observers, in our case update the program frame
+     * @param o     the observable object.
+     * @param arg   an argument passed to the method.
+     */
     @Override
     public void update(Observable o, Object arg) {
         ZooPanel.getInstance().manageZoo();
