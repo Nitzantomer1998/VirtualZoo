e5f4d74f6531a0f4002bc0bc82fb59f45a21a467
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-08-17 10:15:40 +0300

    Adjusting notes

--- VirtualZooCode/designPatterns/IAnimalColor.java
+++ VirtualZooCode/designPatterns/IAnimalColor.java
@@ -1,6 +1,13 @@
 package designPatterns;
 
 
+/**
+ * an Interface to implement the Decorator Design Pattern
+ *
+ * @version 2020.3.3
+ * @author Nitzan Tomer
+ * @see designPatterns.AnimalColorDecorator,graphics.ChangeColorDialog,animals.Animal
+ */
 public interface IAnimalColor {
 
     void PaintAnimal(String color);
