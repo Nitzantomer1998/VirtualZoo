29c01ac02eb30ca8d9d242273da7a22cbb95a5ae
Nitzan Tomer <Nitzantomer1998@gmail.com>
2022-08-18 07:35:37 +0300

    Adjusting notes

--- VirtualZooCode/designPatterns/ICloneable.java
+++ VirtualZooCode/designPatterns/ICloneable.java
@@ -3,6 +3,13 @@ package designPatterns;
 import animals.Animal;
 
 
+/**
+ * an Interface to implement the Clone Design Pattern
+ *
+ * @version 2020.3.3
+ * @author Nitzan Tomer
+ * @see animals.Animal
+ */
 public interface ICloneable {
 
     Animal Clone();
