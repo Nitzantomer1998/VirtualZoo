69e670b87f857d64c5f6253159b55b319e571df2
Nitzan Tomer <Nitzantomer1998@gmail.com>
2023-01-19 09:34:50 +0200

    Adjusting notes

--- VirtualZooCode/graphics/IDrawable.java
+++ VirtualZooCode/graphics/IDrawable.java
@@ -11,7 +11,7 @@ import java.awt.*;
  */
 public interface IDrawable {
 
-    String PICTURE_PATH = System.getProperty("user.dir").concat("\\VirtualZooCode\\projectPictures\\");
+    String PICTURE_PATH = System.getProperty("user.dir").concat("\\ProjectPictures\\");
     void loadImages(String imageName);
     void drawObject (Graphics g);
 }
